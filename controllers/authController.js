a51858fe7d60d21faa15e4adf6fb693f0bdad5f1
Omar Ashraf <Omarhelmy22@example.com>
2023-12-21 17:11:14 +0200

    mfe4 update

--- controllers/authController.js
+++ controllers/authController.js
@@ -271,7 +271,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
-// to be modified ( Done elmafroud helmiii)
+// to be modified ( Done elmafroud)
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
   // 1) Get user from collection
