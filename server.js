4a69b8eed32d13cbb98c3cced89644d29c6a6ad7
Abdelmoez <abmoezashraf@gmail.com>
2023-12-27 17:07:05 +0200

    sign up validators update

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ UserModel.hasMany(reportModel, { constraints: true, onDelete: "CASCADE" });
 reportModel.belongsTo(UserModel, { constraints: true, onDelete: "CASCADE" });
 
 sequelize
-  .sync({ alerts: true })
+  .sync()
   .then((results) => {
     console.log("DB connected");
   })
